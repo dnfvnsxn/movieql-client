a8a5d75dcd8bb22c4fa222eacc2639a1830df62b
dnfvnsxn <dnfvnsxn@naver.com>
2020-01-19 05:17:53 +0900

    type error

--- src/Detail.js
+++ src/Detail.js
@@ -6,21 +6,24 @@ const Detail = ({
     match: {
         params: { movieId }
     }
-}) => (
-    <Query query={MOVIE_DETAILS} variables={{ movieId }}>
-        {({ loading, error, data }) => {
-            if (loading) return "loading";
-            if (error) return "error";
-            return (
-                <React.Fragment>
-                    {data.movie.title}
-                    {data.movie.medium_cover_image}
-                    {data.movie.rating}
-                    {data.movie.description_intro}
-                </React.Fragment>
-            );
-        }}
-    </Query>
-);
+}) => {
+    movieId = parseInt(movieId);
+    return (
+        <Query query={MOVIE_DETAILS} variables={{ movieId }}>
+            {({ loading, error, data }) => {
+                if (loading) return "loading";
+                if (error) return "error";
+                return (
+                    <React.Fragment>
+                        {data.movie.title}
+                        {data.movie.medium_cover_image}
+                        {data.movie.rating}
+                        {data.movie.description_intro}
+                    </React.Fragment>
+                );
+            }}
+        </Query>
+    );
+};
 
 export default Detail;
