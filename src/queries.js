f465fcbc59f84d9757c8c72a1d7364fe43c8a63d
dnfvnsxn <dnfvnsxn@naver.com>
2020-01-19 05:37:50 +0900

    suggestions

--- src/queries.js
+++ src/queries.js
@@ -1,19 +1,19 @@
 import gql from "graphql-tag";
 
 export const HOME_PAGE = gql`
-    query {
-        movies(limit: 50, rating: 8.5) {
+    {
+        movies(limit: 50, rating: 7) {
             id
             title
-            genres
             rating
             medium_cover_image
         }
     }
 `;
+
 export const MOVIE_DETAILS = gql`
     query getMovieDetails($movieId: Int!) {
-        movie(id: ($movieId) {
+        movie(id: $movieId) {
             medium_cover_image
             title
             rating
